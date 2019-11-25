26ea951cb340c20b7f981dd3eb504a3869fe364d
Ahmet DAL <ceahmetdal@gmail.com>
2019-11-25 23:24:38 +0100

    Set up demo settings

---
+++ river_admin_demo/settings.py
@@ -0,0 +1,119 @@
+import os
+import django_heroku
+
+BASE_DIR = os.path.dirname(__file__)
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dgbhuet3o0ndo',
+        'USER': 'jvrabifuuvsbis',
+        'PASSWORD': '89092bd919abc59d289d0e87b7e5378560ea35261449907c323346713a5b3f13',
+        'HOST': 'ec2-54-247-177-254.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432'
+    },
+}
+
+RIVER_INJECT_MODEL_ADMIN = False
+USE_TZ = True
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'river',
+    'river_admin',
+    'river_admin_shipping_example',
+    'river_admin_issue_tracker_example',
+)
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'EXCEPTION_HANDLER': 'river_admin.views.exception_handler'
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+SITE_ID = 1
+
+SECRET_KEY = 'abcde12345'
+
+ROOT_URLCONF = 'local_urls'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'default': {
+            'format': '(%(module)s) (%(name)s) (%(asctime)s) (%(levelname)s) %(message)s',
+            'datefmt': "%Y-%b-%d %H:%M:%S"
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        }
+
+    },
+    'loggers': {
+        'river': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
+
+django_heroku.settings(locals())
