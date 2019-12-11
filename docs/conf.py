37c601e691558bd560d66f90aa336e2401836d52
Ahmet DAL <ceahmetdal@gmail.com>
2019-12-11 16:31:33 +0100

    :arrow_up: Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Ahmet Dal'
 author = 'Ahmet Dal'
 
 # The short X.Y version
-version = '0.5.1'
+version = '0.5.2'
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
